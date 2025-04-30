e873d47e10ea03d1e87f3a040c7dde5af46930b6
Joshua Dowell <joshuadowellcs@gmail.com>
2025-04-30 16:37:12 -0400

    Additional list assignment to practice

---
+++ animals.py
@@ -0,0 +1,11 @@
+# Animal list that stores three different animals
+animals = {'cat','dog','rabbit'}
+
+# Loop iterates through animals in the list
+for animal in animals:
+
+    # Prints each animal that is in the list
+    print('A',animal, 'would make a great pet.')
+
+# Additional statement outside of loop 
+print('Any of these animals would make a great pet')
