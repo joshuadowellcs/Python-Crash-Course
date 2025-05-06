c33b8b18c3d3b3d8ce625465f9375da69594d18b
Joshua Dowell <joshuadowellcs@gmail.com>
2025-05-05 20:13:42 -0400

    Continued with conditional statements

--- Ch5 If Statements/toppings.py
+++ Ch5 If Statements/toppings.py
@@ -1,4 +1,22 @@
-requested_toppings = 'mushrooms'
+"""requested_toppings = ['mushrooms', 'extra cheese']
 
-if requested_toppings != 'achovies':
-    print("Hold the anchovies!")
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")"""
+
+# The above code will not work if using if elif else statements
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
