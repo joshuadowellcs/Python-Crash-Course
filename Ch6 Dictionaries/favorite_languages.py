8dc526fa2f00b1a0760fc2175c282a95de549158
Joshua Dowell <joshuadowellcs@gmail.com>
2025-05-09 20:23:34 -0400

    Continuing with Dictionaries chapter

--- Ch6 Dictionaries/favorite_languages.py
+++ Ch6 Dictionaries/favorite_languages.py
@@ -1,7 +1,40 @@
-favorite_languages = {'jen': 'python', 
+"""favorite_languages = {'jen': 'python', 
                       'sarah': 'c', 
                       'edward': 'rust', 
                       'phil': 'python',}
 
 language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+print(f"Sarah's favorite language is {language}.")"""
+
+# Using for loop with dictionary to find key value pairs
+
+"""favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")"""
+
+# Looping through all keys in a dictionary .keys()
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+friends = ['phil','sarah']
+
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}.")
+        
+# Checks to see if name is not in dictionary
+    if 'erin' not in favorite_languages.keys():
+        print("Erin, please take our poll.")
